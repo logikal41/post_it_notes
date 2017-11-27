c0ab2c68f73ad2e21a0fbf5233b9808dd3e08b17
Matt Berry <mberry41@gmail.com>
2017-11-26 20:50:57 -0700

    added the destroy and update methods / views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,6 +21,24 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
   private
     def note_params
       params.require(:note).permit(:title, :author, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
  
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+
   resources :notes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
